ce77b93f01c58044b378c42df40ef65166d0f693
Greg <Ghughes610@gmail.com>
2021-06-09 19:15:23 -0600

    done

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+class Dog name 
+    
+    def initialize name
+        @name = name
+    end
+end
+
+
+fido = Dog.new(name= "fido")
+snoopy = Dog.new(name= "snoopy")
+lassie = Dog.new(name= "lassie")
